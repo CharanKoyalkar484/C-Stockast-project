6bdfb5e54d9e9ab46055e23d0af23f55a9e026a1
packandsell <edward.samokhvalov@gmail.com>
2019-01-20 18:16:00 +0000

    added build with CMake

---
+++ CMakeFiles/stockast.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stockast.dir/stockast.cpp.o"
+  "stockast.pdb"
+  "stockast"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stockast.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
