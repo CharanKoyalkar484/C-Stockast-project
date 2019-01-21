e3b168da144ead818605eaf568046d524c332afc
packandsell <edward.samokhvalov@gmail.com>
2019-01-21 03:04:04 +0000

    changed some text in stockast.cpp

--- stockast.cpp
+++ stockast.cpp
@@ -5,6 +5,7 @@
 * MIT License
 *
 * Copyright (c) 2017-2019 Rajdeep Konwar
+(with slight modifications by Eduard Samokhvalov)
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
